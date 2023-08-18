25491db6fec9bc41e63f959c8a0fbc3a75730201
x4trm <damian.maza@wp.pl>
2023-08-18 15:26:10 +0200

    fix bug

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'nokogiri'
+require 'open-uri'
 require_relative 'car.rb'
 class Scraper
     attr_reader :cars
@@ -26,8 +27,13 @@ class Scraper
         # url = car.css("div")[1].css("h1").css("a").first.attribute("href").value
         url = car.css("div h1 a").first.attribute("href").value
         name = car.css('h1').css('a').first.text
-        image = car.css("div")[0].css("img").first.attribute("src").value
-        # image = car.css("div img").first.attribute("src").value
+        img_src = car.css("div")[0].css("img").attribute("src")
+        if img_src.nil?
+            image="lack;s=0"
+        else
+            image = img_src
+        end
+          # image = car.css("div img").attribute("src")
         price = car.css('div h3').text
         info = scrape_info(url)
         Car.new(url,image,name,price,info)
